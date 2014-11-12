9bf3412d0793d749b39d648d4aff1cf7401bbb24
ZephireNZ <brynleymcdonald@gmail.com>
2014-11-12 22:46:58 +1300

    Remove cleanup, will be implemented at a later date

--- src/main/java/org/bukkit/inventory/ItemMatcher.java
+++ src/main/java/org/bukkit/inventory/ItemMatcher.java
@@ -10,16 +10,4 @@ public class ItemMatcher {
     public ItemStack getResult(ItemStack[] matrix) {
         return null; // Don't match by default
     }
-
-    /**
-     * Clean the crafting matrix after crafting an item.
-     *
-     * <p>Primarily used in recipes where some of the parts of the recipe need to be preserved.</p>
-     * @param matrix Crafting matrix to clean
-     * @param result Result of the given matrix
-     */
-    public void cleanup(ItemStack[] matrix, ItemStack result) {
-        // Do nothing by default
-    }
-
 }
